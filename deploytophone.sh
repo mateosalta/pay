94bc7a492dc6383bd45f07b939a853e180fa0341
mateosalta <mateo_salta@yahoo.com>
2021-05-28 09:57:57 -0500

    Delete deploytophone.sh

--- deploytophone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm *.click
-click build ../"${PWD##*/}" 
-adb shell "rm ~/Documents/*.click"
-adb push *.click Documents
-adb shell "pkcon install-local ~/Documents/*.click --allow-untrusted"
-
